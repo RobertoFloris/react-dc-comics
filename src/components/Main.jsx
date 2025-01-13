eb4e79e99e22661f04aaa00f2b7840d96fd09697
RobertoFloris <florisroberto99@gmail.com>
2025-01-13 19:19:05 +0100

    draft main layout

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,72 @@
 const Main = () => {
   return (
-    <main>Main</main>
+    <main>
+      <section>
+
+        <div>
+          <div className="container">
+            <h2>&lt;-- Content goes here --&gt;</h2>
+          </div>
+        </div>
+
+        <div>
+          <div className="container">
+            <ul>
+              <li><a href="#">DIGITALS COMICS</a></li>
+              <li><a href="#">DC MERCHANDISE</a></li>
+              <li><a href="#">SUBSCRIPTION</a></li>
+              <li><a href="#">COMIC SHOP LOCATOR</a></li>
+              <li><a href="#">DC POWER VISA</a></li>
+            </ul>
+          </div>
+        </div>
+
+      </section>
+
+      <section>
+        <div className="container">
+          <div>
+            <ul className="link">
+              <h2><a href="#">DC COMICS</a></h2>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+
+              <h2><a href="#">SHOP</a></h2>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+
+              <h2><a href="#">DC</a></h2>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy (New)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+
+              <h2><a href="#">SITES</a></h2>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
