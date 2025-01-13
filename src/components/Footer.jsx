e562bcc753d81ccb175d75781bf9488eaacfc81a
RobertoFloris <florisroberto99@gmail.com>
2025-01-13 21:29:11 +0100

    draft footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,30 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className="container">
+        <div>
+          <button>SIGN-UP NOW!</button>
+        </div>
+
+        <div className="flex">
+
+          <div>
+            <h3>FOLLOW US</h3>
+          </div>
+
+
+          <div>
+            <ul>
+              <li><a href="#">F</a></li>
+              <li><a href="#">T</a></li>
+              <li><a href="#">Y</a></li>
+              <li><a href="#">P</a></li>
+              <li><a href="#">P</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
