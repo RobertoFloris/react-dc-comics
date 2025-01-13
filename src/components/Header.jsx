37622e0849a66491417cd0fc71c95558d1549991
RobertoFloris <florisroberto99@gmail.com>
2025-01-13 16:36:30 +0100

    draft header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,27 @@
 const Header = () => {
   return (
-    <header>Header</header>
+    <header>
+      <div className="container">
+        <div>
+          LOGO
+        </div>
+        <div>
+          <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </div>
+      </div>
+
+    </header>
   )
 }
 
