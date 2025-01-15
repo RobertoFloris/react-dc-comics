6ef42e16e6b03d7560073f9c0e9e0c54aa905877
RobertoFloris <florisroberto99@gmail.com>
2025-01-15 19:42:12 +0100

    [refact] layout list in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 import BlueBand from "./partials/BlueBand"
 import Social from "./partials/Social"
 
-const Footer = () => {
+const Footer = (props) => {
+
+  // const { footerList } = props;
+
   return (
     <footer>
 
@@ -10,41 +13,55 @@ const Footer = () => {
         <BlueBand />
 
         <div className="container">
-          <div>
-            <ul className="link">
-              <h2>DC COMICS</h2>
-              <li><a href="#">Characters</a></li>
-              <li><a href="#">Comics</a></li>
-              <li><a href="#">Movies</a></li>
-              <li><a href="#">TV</a></li>
-              <li><a href="#">Games</a></li>
-              <li><a href="#">Videos</a></li>
-              <li><a href="#">News</a></li>
 
-              <h2>SHOP</h2>
-              <li><a href="#">Shop DC</a></li>
-              <li><a href="#">Shop DC Collectibles</a></li>
+          <div className="link flex">
 
-              <h2>DC</h2>
-              <li><a href="#">Terms Of Use</a></li>
-              <li><a href="#">Privacy Policy (New)</a></li>
-              <li><a href="#">Ad Choices</a></li>
-              <li><a href="#">Advertising</a></li>
-              <li><a href="#">Jobs</a></li>
-              <li><a href="#">Subscriptions</a></li>
-              <li><a href="#">Talent Workshops</a></li>
-              <li><a href="#">CPSC Certificates</a></li>
-              <li><a href="#">Ratings</a></li>
-              <li><a href="#">Shop Help</a></li>
-              <li><a href="#">Contact Us</a></li>
+            <div>
+              <ul>
+                <li><h2>DC COMICS</h2></li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+              <ul className="link">
+                <li><h2>SHOP</h2></li>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
 
-              <h2>SITES</h2>
-              <li><a href="#">DC</a></li>
-              <li><a href="#">MAD Magazine</a></li>
-              <li><a href="#">DC Kids</a></li>
-              <li><a href="#">DC Universe</a></li>
-              <li><a href="#">DC Power Visa</a></li>
-            </ul>
+            <div>
+              <ul>
+                <li><h2>DC</h2></li>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy Policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+
+            <div>
+              <ul>
+                <li><h2>SITES</h2></li>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
+
           </div>
           <div>
             <img src="img/dc-logo-bg.png" alt="logoBg" />
